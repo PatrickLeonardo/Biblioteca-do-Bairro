cb4b9e67696a16f18a59f084966981675058b4cc
PatrickLeonardo <patricklf7@proton.me>
2024-09-01 13:55:16 -0300

    testing kernel

--- assembler.sh
+++ assembler.sh
@@ -2,15 +2,13 @@
 {
 	clear
 	echo "Assembling bootloader file..."	
-	nasm -f bin bootloader.asm -l bootloader.bin
+	nasm -f bin bootloader.asm -o bootloader.bin
 	read -rst 1; timeout=$?
 
 	echo "Assembling kernel file..."
-	nasm -f bin kernel.asm -l kernel.bin
+	nasm -f bin kernel.asm -o kernel.bin
 	read -rst 1; timeout=$?
 
-	dd if=bootloader.bin of=OS.img bs=512 seek=1 conv=notrunc
-	dd if=kernel.bin of=OS.img bs=512 seek=2 conv=notrunc
 	echo "Done! Press enter..."
 	read -rs
 	clear
